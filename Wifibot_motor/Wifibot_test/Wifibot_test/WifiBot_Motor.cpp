982f3b8696c0a22135d7be60efaf17d7a0399e68
FRE3X <Callot.antoine@gmail.com>
2014-09-27 13:01:38 +0200

    mise en place d'une métode de Calcule du CRC

--- Wifibot_motor/Wifibot_test/Wifibot_test/WifiBot_Motor.cpp
+++ Wifibot_motor/Wifibot_test/Wifibot_test/WifiBot_Motor.cpp
@@ -21,3 +21,27 @@ WifiBot_Motor::WifiBot_Motor(unsigned char speed1,unsigned char speed2,unsigned 
 WifiBot_Motor::~WifiBot_Motor(void)
 {
 }
+
+short WifiBot_Motor::Crc16(unsigned char *Adresse , unsigned char taille_max){
+	unsigned int Crc = 0xFFFF;
+	unsigned int Polynome = 0xA001;
+	unsigned int CptOctet = 0;
+	unsigned int CptBit = 0;
+	unsigned int Parity= 0;
+	
+	Crc = 0xFFFF;
+	Polynome = 0xA001;
+	
+	for ( CptOctet= 0 ; CptOctet < taille_max ; CptOctet++)
+	{
+		Crc ^= *( Adresse + CptOctet);
+		for ( CptBit = 0; CptBit <= 7 ; CptBit++)
+		{
+			Parity= Crc;
+			Crc >>= 1;
+			if (Parity%2 == true) Crc ^= Polynome;
+		}
+	}
+	return(Crc);
+
+}
--- Wifibot_motor/Wifibot_test/Wifibot_test/WifiBot_Motor.h
+++ Wifibot_motor/Wifibot_test/Wifibot_test/WifiBot_Motor.h
@@ -6,6 +6,9 @@ public:
 	WifiBot_Motor(void);
 	WifiBot_Motor(unsigned char,unsigned char,unsigned char); 
 	~WifiBot_Motor(void);
+
+	//méthode pour calculée le CRC
+	short Crc16(unsigned char *Adresse_tab , unsigned char Taille_max); 
 	
 };
 
